4eb4bf4025e21a865796dda91d982f5d1590fd13
sayantan <sayantan.roy123789@gmail.com>
2023-08-03 22:31:11 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <div>
+        <h1>About us</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {" "}
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi,
+          nostrum{" "}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam, id.
+          Quas excepturi asperiores harum eius at explicabo aliquam ipsam culpa
+          nam cum, doloribus laborum reiciendis perferendis mollitia vero,
+          itaque, sapiente magni. Cum necessitatibus suscipit porro iure ad
+          ullam sapiente doloremque quibusdam rem delectus, fuga culpa omnis
+          eius adipisci quos sequi beatae expedita? Quis molestiae accusamus,
+          earum magnam quia qui. Ea culpa, amet quod deleniti impedit ex alias
+          soluta deserunt, quia doloribus vitae eos temporibus. Inventore
+          nesciunt officia saepe illo hic suscipit dolores dicta? Asperiores
+          delectus, accusantium, placeat sit aliquam vel dolor eaque enim
+          doloremque velit mollitia quo amet eius nesciunt!
+        </p>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor dolore
+          quidem quis eaque expedita velit, officia cumque aliquid architecto.
+          Ullam!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio,
+          commodi______ .
+        </p>
+      </div>
     </div>
   );
 }
